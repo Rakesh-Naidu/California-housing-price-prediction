b6302cc8312660e1cb47be4a37c07e163f83fd28
Rakesh <44801151+Rakesh-Naidu@users.noreply.github.com>
2021-02-01 15:49:35 +0530

    Add files via upload

--- app.py
+++ app.py
@@ -11,13 +11,13 @@ def home():
 
 @app.route('/predict', methods=['POST'])
 def predict():
-    features = [int(x) for x in request.form.values()]
+    features = [float(x) for x in request.form.values()]
     t = features[1]
     features[1] = features[2]
     features[2] = t
     final_feat = [np.array(features)]
     prediction = model.predict(final_feat)
-    return render_template('index.html', prediction_text = "The price of the house is : ${}".format(prediction))
+    return render_template('index.html', prediction_text = "The price of the house is : ${}".format(prediction[0]))
 
 if __name__ == "__main__":
     app.run(debug=True)
